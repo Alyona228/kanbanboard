bbf7af614c7717cf82e0dcd5daf4256bd8d6cc6f
Name <56693963+Alyona228@users.noreply.github.com>
2021-01-23 22:57:01 +0300

    dizable menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,11 @@ import avatar from '../images/user-avatar.svg'
 import arrow from '../images/arrow-down.svg'
 
 function Header(props) {
-  const [visable, setVisable] = useState(false)
-  const togleselect = () => setVisable(!visable)
-
+  let [visable, setVisable] = useState(false)
+  let toggleselect = () => setVisable(!visable)
+  let onChangeSelect = (event) => {
+    setVisable(false)
+  }
   return (
     <div className='header'>
       <div>
@@ -16,14 +18,17 @@ function Header(props) {
         <img src={avatar} alt='это аватар' />
       </div>
 
-      <button className='header__button'>
+      <button className='header__button' onClick={toggleselect}>
         <img src={arrow} alt='это аватар' className='header__arrow' />
       </button>
-      <div className='menu'>
-        <div className='menu_text'></div>
-        <div className='menu_text'></div>
-        <div className='menu_text'></div>
-      </div>
+
+      {!visable && (
+        <div className='menu'>
+          <div className='menu_text'></div>
+          <div className='menu_text'></div>
+          <div className='menu_text'></div>
+        </div>
+      )}
     </div>
   )
 }
